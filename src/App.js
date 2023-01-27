c25ee32214f43219d225289fd9ad670d47ce7069
VaibhavR02 <vaibhavrandale800@gmail.com>
2023-01-28 00:24:52 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
+import PreNavbar from './components/PreNavbar';
+import Navbar from './components/Navbar.jsx';
+import Slider from './components/Slider.jsx';
+import Offers from './components/Offers.jsx';
+import Heading from './components/Heading.jsx';
+import StarProducts from './components/StarProducts.jsx';
+import HotAccessoriesMenu from './components/HotAccessoriesMenu.jsx';
+import HotAccessories from './components/HotAccessories.jsx';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import data from './data/data.json';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <PreNavbar />
+      <Navbar />
+      <Slider start={data.banner.start} />
+      <Offers offer={data.offer} />
+      <Heading text="STAR PRODUCTS" />
+      <StarProducts starProduct={data.starProduct} />
+      <Heading text="HOT ACCESSORIES" />
+      <HotAccessoriesMenu />
+
+      <Routes>
+        <Route
+          path="/music"
+          element={
+            <HotAccessories
+              music={data.hotAccessories.music}
+              musicCover={data.hotAccessoriesCover.music}
+            />
+          }
+        />
+        <Route
+          path="/smartDevice"
+          element={
+            <HotAccessories
+              smartDevice={data.hotAccessories.smartDevice}
+              smartDeviceCover={data.hotAccessoriesCover.smartDevice}
+            />
+          }
+        />
+        <Route
+          path="/home"
+          element={
+            <HotAccessories
+              home={data.hotAccessories.home}
+              homeCover={data.hotAccessoriesCover.home}
+            />
+          }
+        />
+        <Route
+          path="/lifeStyle"
+          element={
+            <HotAccessories
+              lifeStyle={data.hotAccessories.lifeStyle}
+              lifeStyleCover={data.hotAccessoriesCover.lifeStyle}
+            />
+          }
+        />
+        <Route
+          path="/mobileAccessories"
+          element={
+            <HotAccessories
+              mobileAccessories={data.hotAccessories.mobileAccessories}
+              mobileAccessoriesCover={
+                data.hotAccessoriesCover.mobileAccessories
+              }
+            />
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Slider.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import item from './amazon.jpg';
+const Slider = ({ start }) => {
+  return (
+    <Carousel fade>
+      {start.map((item, index) => (
+        <Carousel.Item key={index}>
+          <img className="d-block w-100" src={item} alt="First slide" />
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
+};
+
+export default Slider;
